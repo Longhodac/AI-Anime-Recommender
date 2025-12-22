f5e09ca8933cbc520705d44095d5a88057c0a74c
longhodac <long110404@gmail.com>
2025-12-22 00:34:49 -0800

    added recommendation pipeline

--- utils/logger.py
+++ utils/logger.py
@@ -1,16 +1,19 @@
-import sys
+import logging
+import os
+from datetime import datetime
 
-class CustomException(Exception):
-    def __init__(self, message: str, error_detail: Exception = None):
-        self.error_message = self.get_detailed_error_message(message, error_detail)
-        super().__init__(self.error_message)
+LOGS_DIR = "logs"
+os.makedirs(LOGS_DIR,exist_ok=True)
 
-    @staticmethod
-    def get_detailed_error_message(message, error_detail):
-        _, _, exc_tb = sys.exc_info()
-        file_name = exc_tb.tb_frame.f_code.co_filename if exc_tb else "Unknown File"
-        line_number = exc_tb.tb_lineno if exc_tb else "Unknown Line"
-        return f"{message} | Error: {error_detail} | File: {file_name} | Line: {line_number}"
+LOG_FILE = os.path.join(LOGS_DIR, f"log_{datetime.now().strftime('%Y-%m-%d')}.log")
 
-    def __str__(self):
-        return self.error_message
+logging.basicConfig(
+    filename=LOG_FILE,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    return logger
